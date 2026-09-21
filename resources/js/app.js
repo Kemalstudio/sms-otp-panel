388d5e18257fc8c9342349d1e9078ce235a6945f
Kemal Atayev <kemalatayew913@gmail.com>
2026-09-21 15:28:23 +0500

    1.509

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,11 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import gatewayPulse from './pulse';
 
 window.Alpine = Alpine;
+
+// Компонент живых счётчиков доступен вьюхам как x-data="gatewayPulse(...)".
+Alpine.data('gatewayPulse', gatewayPulse);
 
 Alpine.start();
